76302072b475372bea5aaac7e6a1727fa28308c6
Kirill Gutorov <gutorovvvvv.k@gmail.com>
2024-02-05 18:32:38 +0700

    last changes

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("The initial opening is expected in 45 days");
+        System.out.println("The initial opening is expected in 54 days");
         System.out.println("something new is added");
+        System.out.println("some new changes");
     }
 }
