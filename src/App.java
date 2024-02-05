503edaac326f707c489e9b499e898f46a6fa1e93
Kirill Gutorov <gutorovvvvv.k@gmail.com>
2024-02-05 18:03:14 +0700

    added some text and changed the date of release

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("The initial opening is expected in 4 days");
+        System.out.println("The initial opening is expected in 45 days");
+        System.out.println("something new is added");
     }
 }
